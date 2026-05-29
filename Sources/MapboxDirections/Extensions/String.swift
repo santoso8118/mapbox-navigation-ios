03d9076fbbfdb0a38395aabd19d602835ea49a41
Santoso Pham <tuan.pham5@ntq-solution.com.vn>
2026-05-29 16:07:36 +0700

    fix(mapbox): Fix build error

--- Sources/MapboxDirections/Extensions/String.swift
+++ Sources/MapboxDirections/Extensions/String.swift
@@ -2,10 +2,10 @@ import Foundation
 
 extension String {
     var nonEmptyString: String? {
-        var text self.trimmingCharacters(in: .whitespacesAndNewlines)
+        var text = self.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
         while text.last == "/" {
             text.removeLast()
-            text = text.trimmingCharacters(in: .whitespacesAndNewlines)
+            text = text.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
         }
         return !text.isEmpty ? text : nil
     }
